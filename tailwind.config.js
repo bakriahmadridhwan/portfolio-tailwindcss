52a9b0c6be129c06ccdaa8a9109131f391da47ca
bakriahmadridhwan <bahridhwan@gmail.com>
2023-01-24 05:24:38 +0700

    ic

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ['./index.html'],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      animation: {
+        'slow-spin': 'spin 5s linear infinite'
+      },
+      colors: {
+        primary: '#14b8a6',
+        secondary: '#64748b',
+        dark: '#0f172a',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
